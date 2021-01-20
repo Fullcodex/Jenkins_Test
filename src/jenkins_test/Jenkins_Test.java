03ca0566f355fe6d104e98cad71892041f15a889
FullCodex <fullcodex974@gmail.com>
2021-01-20 10:08:14 +0400

    1.0.1

--- src/jenkins_test/Jenkins_Test.java
+++ src/jenkins_test/Jenkins_Test.java
@@ -17,6 +17,7 @@ public class Jenkins_Test {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Test jenkins");
+        System.out.println("Test jenkins")
     }
     
 }
